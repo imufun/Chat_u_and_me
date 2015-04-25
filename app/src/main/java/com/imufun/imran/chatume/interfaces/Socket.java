2d3bef34cb653db5410d6ed51c55ff44e9180da4
imufun <imufun1@gmail.com>
2015-04-25 10:37:24 +0600

    socket

---
+++ app/src/main/java/com/imufun/imran/chatume/interfaces/Socket.java
@@ -0,0 +1,13 @@
+package com.imufun.imran.chatume.interfaces;
+
+/**
+ * Created by imran on 4/25/15.
+ */
+public interface Socket {
+
+    public String sendHTTPRequest(String Params);
+    public int startListeningPort(int Port);
+
+    public void stopListening();
+    public void exit();
+}
